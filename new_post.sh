63f687d3cb2608cebfc92c10f2b41f6546300487
Tadashi_air <lhomme.blanc@me.com>
2016-03-01 19:15:15 +0900

    first commit

---
+++ new_post.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# ---
+# layout: post
+# title:
+# date:   2016-03-01 11:32:52 +0900
+# ---
+
+if test $# -ne 1; then
+    echo "pleas post name"
+    return 0
+fi
+
+frontMatterLine=---
+layout=layout:post
+title=title:
+date=date:`date "+%Y-%m-%d %H:%M:%S +0900"`
+file=_posts/`date +%Y-%m-%d`-${1}.md
+
+touch ${file}
+
+echo $frontMatterLine > $file
+echo $layout >> $file
+echo $title >> $file
+echo $date >> $file
+echo $frontMatterLine >> $file
